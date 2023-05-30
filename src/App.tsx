bc5d70c730756e0a4773310be8f5ee7429a61c36
Laurent Kvb <laurent.kleeringvanbeerenbergh@leaseplan.com>
2023-05-30 11:08:45 +0200

    add loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import "./App.css";
 import { VideoDetail } from "./components/VideoDetail";
 import { getYoutubeId, VideoDTO, fetchVideos } from "./App.model";
 
+import spinner from "./assets/spinner.gif";
 export const App: FC = () => {
   const [videos, setVideos] = useState<VideoDTO[]>([]);
+  const [loading, setLoading] = useState(true);
   const [currentVideoId, setCurrentVideoId] = useState("");
 
   useEffect(() => {
     fetchVideos()
       .then((data) => setVideos(data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
 
   const handleTileClick = (videoId: string): void => {
@@ -21,16 +24,25 @@ export const App: FC = () => {
   return (
     <div className="app">
       <h1>Video app</h1>
-      <div className="video-gallery">
-        {videos.map((video) => (
-          <VideoTile
-            key={video.feedid}
-            title={video.title}
-            imageUrl={video.image}
-            onClick={() => handleTileClick(getYoutubeId(video.sources[0].file))}
-          />
-        ))}
-      </div>
+      {loading && (
+        <div className="spinner">
+          <img src={spinner} alt="spinner" />
+        </div>
+      )}
+      {!loading && (
+        <div className="video-gallery">
+          {videos.map((video) => (
+            <VideoTile
+              key={video.feedid}
+              title={video.title}
+              imageUrl={video.image}
+              onClick={() =>
+                handleTileClick(getYoutubeId(video.sources[0].file))
+              }
+            />
+          ))}
+        </div>
+      )}
       {currentVideoId && (
         <VideoDetail
           videoId={currentVideoId}
